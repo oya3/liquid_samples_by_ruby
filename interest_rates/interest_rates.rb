507bfb05218e7b12246f7ce3477630ce4f280db3
Kazunori OYA <terje@oya3.net>
2018-11-12 15:08:02 +0900

    add sample for interest_rates

---
+++ interest_rates/interest_rates.rb
@@ -0,0 +1,25 @@
+require 'uri'
+require 'net/http'
+require 'time'
+require 'json'
+require 'yaml'
+require 'pry'
+
+# URI
+uri = URI.parse("https://api.liquid.com")
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+
+# PATH
+currency_name = 'USD'
+path = URI.parse("/ir_ladders/#{currency_name}")
+request = Net::HTTP::Get.new(path.to_s)
+request.add_field('X-Quoine-API-Version', '2')
+response = http.request(request)
+if response.code != '200'
+  print "error:#{response}"
+  exit
+end
+interest_rates = JSON.parse(response.body)
+File.write 'interest_rates.json', interest_rates.to_json
+File.write 'interest_rates.yml', interest_rates.to_yaml
---
+++ interest_rates/readme.txt
@@ -0,0 +1,4 @@
+$ bundle install --path vendor/bundle
+
+# interest_rates を取得する
+$ bundle exec ruby interest_rates.rb
