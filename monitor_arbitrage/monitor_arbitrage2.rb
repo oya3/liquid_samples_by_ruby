b40056682f91a1b5b39b36d4559b5f9b6bd13c59
Kazunori OYA <terje@oya3.net>
2018-11-13 14:26:00 +0900

    append pri-doc,pry-byebug to Gemfile

--- monitor_arbitrage/monitor_arbitrage2.rb
+++ monitor_arbitrage/monitor_arbitrage2.rb
@@ -5,71 +5,64 @@ require 'json'
 require 'yaml'
 require 'pry'
 
-# BTC/JPY
-#  "id": "5",
-#  "market_ask": 724729.95, # 売値
-#  "market_bid": 724684.0, # 買値
-#  "last_traded_price": "724735.87"
-
-# QASH/BTC
-#  "id": "52",
-#  "market_ask": 3.485e-05,
-#  "market_bid": 3.45e-05,
-#  "last_traded_price": "0.0000345",
-
-# QASH/JPY
-#  "id": "50",
-#  "market_ask": 25.28,
-#  "market_bid": 25.06,
-#  "last_traded_price": "25.06",
-
-
 # URI
 uri = URI.parse("https://api.liquid.com")
 http = Net::HTTP.new(uri.host, uri.port)
 http.use_ssl = true
 
 # PATH
-ids = [ 5, 52, 50 ]
-results = Array.new
-count = 1
-while true do
-  start_time = Time.now.to_i
-  path = URI.parse("/products")
-  request = Net::HTTP::Get.new(path.to_s)
-  request.add_field('X-Quoine-API-Version', '2')
-  response = http.request(request)
-  if response.code != '200'
-    print "error:#{response}"
-    exit
-  end
-  products = JSON.parse(response.body)
-  # BTC/JPY
-  # QASH/BTC
-  # QASH/JPY
-  # (1) JPYで買えるコインを検索
-  # (2) (1)で見つけたコインがJPY以外の別コインの購入可能コインを検索
-  products.each do |product|
-    next if product["cfd_enabled"] == 'false'
-    next product["quoted_currency"] != 'JPY'
-    results
-    # product["cfd_enabled"]: false
-    # product["currency_pair_code"] = "JPY"
-    # BTC/JPY
-    # "quoted_currency": "JPY",
-    # "base_currency": "BTC",
-    
-  end
+path = URI.parse("/products")
+request = Net::HTTP::Get.new(path.to_s)
+request.add_field('X-Quoine-API-Version', '2')
+response = http.request(request)
+if response.code != '200'
+  print "error:#{response}"
+  exit
+end
+products = JSON.parse(response.body)
+
+# (1) JPY で購入できるコイン検索
+coin_infos = Hash.new
+pair_products = Hash.new
+products.each do |product|
+  next if product["volume_24h"] == "0.0" # 日本からの取引が24時間ない場合は取扱対象外とする
   
+  currency_pair_code = product['currency_pair_code']
+  if pair_products.has_key? currency_pair_code
+    print "ERROR: multiple currency_pair_code. [#{currency_pair_code}]\n"
+    next
   end
-  end_time = Time.now.to_i
-  # 1:BTC=QASH:x
-  unit = results[0]['last_traded_price'].to_f * results[1]['last_traded_price'].to_f
+  pair_products[currency_pair_code] = product.clone # currency_pair_code 別 porducts に詰める
   
-  arbitrage = "[QASH: #{results[2]['last_traded_price'].to_f}, BTC:#{unit}]"
-  rate = results[2]['last_traded_price'].to_f / unit
-  price = "#{results[0]['last_traded_price']}, #{results[1]['last_traded_price']}, #{results[2]['last_traded_price']}"
-  print("#{count}:#{Time.at(start_time)}-#{Time.at(end_time)}: #{rate}: #{arbitrage}: #{price}\n")
-  count += 1
-  sleep(10) # 10 sec
+  next if product["quoted_currency"] != 'JPY'
+  base_currency = product['base_currency']
+  if coin_infos.has_key? base_currency
+    print "ERROR: multiple base_currency. [#{base_currency}]\n"
+    next
+  end
+  coin_infos[base_currency] = Hash.new
+  coin_infos[base_currency]['product_id'] = product['id']
+  coin_infos[base_currency]['pair_products'] = Array.new
+end
+
+coin_infos.each do |name1, info|
+  coin_infos.each_key do |name2|
+    next if name1 == name2
+    currency_pair_code = "#{name2}#{name1}"
+    next if not pair_products.has_key? currency_pair_code
+    pair_product = pair_products[currency_pair_code]
+    product = {
+      'product_id' => pair_product['id'],
+      'currency_pair_code' => pair_product['currency_pair_code'],
+      'base_currency' => pair_product['base_currency']
+    }
+    info['pair_products'] << product
+  end
+end
+  
+print JSON.pretty_generate(coin_infos) + "\n"
+coin_infos.each do |name1,info|
+  info['pair_products'].each do |info2|
+    print 'JPY-' + name1 + '-' + info2['base_currency'] + "\n"
+  end
 end
